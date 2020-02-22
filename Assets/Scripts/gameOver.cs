4ef569ac8f7b5f97753d62c3372ab2da1e30ff17
ege-yildirim <ege.yildirimm24@gmail.com>
2020-02-22 19:59:43 +0300

    Initial Commit
    Color Bump 3D Replica

---
+++ Assets/Scripts/gameOver.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour
+{
+
+    private bool gameIsOver = false;
+    private bool levelCompleted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameIsOver==true || transform.position.y < 1.9) SceneManager.LoadScene("gameOver");
+
+        else if (levelCompleted ==true) SceneManager.LoadScene("levelComplete");
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "enemy")
+        {
+
+            gameIsOver = true;
+            
+             
+
+
+        }
+
+        if(collision.gameObject.tag == "levelComplete")
+        {
+
+            levelCompleted = true;
+
+        }
+    }
+
+  
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public GameObject player;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player.transform.position += player.transform.forward * Time.deltaTime * speed;
+        
+
+        if(Input.GetKey("left"))
+        {
+            player.transform.position += -transform.right * Time.deltaTime * speed;
+
+
+        }
+
+        if(Input.GetKey("right"))
+        {
+
+            player.transform.position += transform.right * Time.deltaTime * speed;
+
+        }
+
+        
+    }
+}
